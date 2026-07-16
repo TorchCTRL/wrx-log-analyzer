3346851e2892fe9a3e336515862f60d0e6c0d6b1
Efieson Zeamuru <efiesonzeamuru@gmail.com>
2026-07-15 22:05:50 -0400

    Create initial SwiftUI application

---
+++ WRXLogAnalyzer/WRXLogAnalyzer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WRXLogAnalyzer
+//
+//  Created by Efieson Zeamuru on 7/15/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("God's, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WRXLogAnalyzer/WRXLogAnalyzer/WRXLogAnalyzerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WRXLogAnalyzerApp.swift
+//  WRXLogAnalyzer
+//
+//  Created by Efieson Zeamuru on 7/15/26.
+//
+
+import SwiftUI
+
+@main
+struct WRXLogAnalyzerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
